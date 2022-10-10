4e79b20ef596991d54632321978d6a67a28564f1
codeking <1094234526@qq.com>
2022-10-10 14:42:43 +0800

    2022 10.10 commit 完成数据表单的组件功能
    Signed-off-by: codeking <1094234526@qq.com>

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,66 +1,157 @@
 <template>
-
-<el-input v-model="add_form.temperature"/>
-<el-input v-model="add_form.boiling_point"/>
-  <el-upload
-      ref="uploadFile"
-      :limit="1"
-      action="http://127.0.0.1:8000/test/"
-      :on-exceed="handleExceed"
-      :on-change="handleChange"
-      accept=".gjf"
-      class="left-view"
-      :auto-upload="false"
-  >
-    <template #trigger>
-      <el-button type="primary">选择文件</el-button>
-    </template>
-    <el-button class="ml-3" type="success" @click="submitUpload">
-      提交
-    </el-button>
-    <template #tip>
-      <div class="el-upload__tip text-red">
-        limit 1 file, new file will cover the old file
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>饱和蒸汽压模型</span>
       </div>
     </template>
-  </el-upload>
-{{Pred_result}}
+    <el-form ref="formRef" :model="add_form" label-width="100px" class=""
+             size="large"
+             label-position="right">
+      <el-form-item label="温度" prop="temperature" :rules="[
+                        { required: true, message: '这个是必填的！' },
+                        { type: 'number', message: '请输入数字！' },]"
+      >
+        <el-input v-model.number="add_form.temperature" type="text" autocomplete="off"
+                  input-style="width :150px"/>
+      </el-form-item>
+      <el-form-item label="沸点" prop="boiling_point" :rules="[
+                        { required: true, message: '这个是必填的！' },
+                        { type: 'number', message: '请输入数字！' },]"
+      >
+        <el-input v-model.number="add_form.boiling_point" type="text" autocomplete="off"
+                  input-style="width :150px"/>
+      </el-form-item>
+      <el-form-item label="gjf文件" :rules="[
+                        { required: true, message: '请选择文件' },
+                        { type: 'file', message: '请选择文件' },]"
+      >
+        <el-upload
+            style="width: 150px"
+            ref="uploadFile"
+            :limit="1"
+            :on-exceed="handleExceed"
+            :on-change="handleChange"
+            :on-remove="handleRemove"
+            accept=".gjf"
+            class="left-view"
+            :auto-upload="false"
+        >
+          <template #trigger>
+            <el-button type="primary">选择文件</el-button>
+          </template>
+          <template #tip>
+            <div class="el-upload__tip text-red">
+            </div>
+          </template>
+        </el-upload>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(formRef)">提交</el-button>
+        <el-button @click="resetForm(formRef)">重置</el-button>
+      </el-form-item>
+      {{ Pred_result }}
+    </el-form>
+  </el-card>
 </template>
 
 <script lang="ts" setup>
 import {inject, onMounted, reactive, ref, watch} from "vue";
+import {ElMessage, genFileId} from "element-plus";
 
 const axios = inject("axios")
-const Pred_result=ref()
+// 后台返回的数据
+const Pred_result = ref()
+// 上传的文件
+const uploadFile = ref()
+// 用来校验表单
+const formRef = ref()
 
 const add_form = reactive({
   temperature: null,
   boiling_point: null,
 })
 let fd = reactive(new FormData())
-watch(add_form,()=>{
-  fd.set('temperature',add_form.temperature)
-  fd.set('boiling_point',add_form.boiling_point)
+watch(add_form, () => {
+  fd.set('temperature', add_form.temperature)
+  fd.set('boiling_point', add_form.boiling_point)
 })
 
 const handleChange = async (file) => {
-  fd.set('file',file.raw)
-}
+  if (file.name.split('.')[1] !== 'gjf') {
+    ElMessage.error('只能上传Gjf格式的文件');//限制文件类型
+    uploadFile.value!.clearFiles()
+  }else {
+    fd.set('file', file.raw)
+    formRef.value.file=file
+  }
 
-const submitUpload = async () => {
-  let result = await axios.post('/test/', fd)
-  console.log(result.data.result)
-  if(result.status==200) {
-    Pred_result.value=result.data.result
+}
+// 覆盖上一个文件
+const handleExceed = (files) => {
+  if (files[0].name.split('.')[1] !== 'gjf') {
+    ElMessage.error('只能上传Gjf格式的文件');//限制文件类型
+    uploadFile.value!.clearFiles()
+  }else{
+    uploadFile.value!.clearFiles()
+    const file = files[0]
+    file.uid = genFileId()
+    uploadFile.value!.handleStart(file)
   }
 }
+// 处理移除文件时候的操作
+const handleRemove=(file)=>{
+  fd.delete('file')
+}
 
-onMounted(()=>{
-  fd.set('temperature',add_form.temperature)
-  fd.set('boiling_point',add_form.boiling_point)
+const submitForm = (formEl) => {
+  if (!formEl) return
+  formEl.validate(async (valid) => {
+    if (valid) {
+      if(fd.get('file')!==null){
+        console.log('ok')
+        let result = await axios.post('/test/', fd)
+        console.log(result.data.result)
+        if (result.status == 200) {
+          Pred_result.value = result.data.result
+        }else{
+          // todo 返回有误处理
+          return
+        }
+      }
+      // todo 做一个限流处理
+      ElMessage.error('请选择上传文件！');
+      return
+    } else {
+      return false
+    }
+  })
+}
+
+const resetForm = (formEl) => {
+  if (!formEl) return
+  formEl.resetFields()
+  uploadFile.value!.clearFiles()
+  fd.delete('file')
+}
+
+onMounted(() => {
+  fd.set('temperature', add_form.temperature)
+  fd.set('boiling_point', add_form.boiling_point)
 })
+
 </script>
 
 <style lang="less" scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+.box-card {
+  margin: 50px;
+  width: 480px;
+}
 </style>
